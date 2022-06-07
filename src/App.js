b72f311d6ecb52d74d6e744d5d5befc20f0804b8
Sahilgitb <sahilpawar9975@gmail.com>
2022-06-07 12:17:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, {Component } from 'react'
+import Task from './Task'
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    items: [],
+    text:""
+  }
+  handleChange = e => {
+    this.setState({text:e.target.value})
+  }
+  handleAdd = e =>{
+    if(this.state.text !== "") {
+      const items = [...this.state.items, this.state.text];
+      this.setState({ items: items, text:""});
+    }
+  }
+
+  handleDelete = id => {
+    console.log("Deleted", id);
+    const Olditems = [...this.state.items]
+    console.log("Olditems", Olditems);
+    const items = Olditems.filter((elemenet, i) => {
+      return i !== id
+    })
+    this.setState({ items: items });
+  }
+  render() {
+    return (
+      <div className="container-fluid my-5">
+        <div className="row">
+        <div className="col-sm-6 mx-auto text-white shadow-lg p-3" >  
+          <h2 className="text-center"> Task List </h2>
+          <div className="raw">
+            <div className="col-9">
+              <input type="text" className="form-control"
+              placeholder="Add new Task" value = { this.
+                state.text} onChange= {this.handleChange}/>
+            </div>
+            <div className="col-2">
+              <button className="btn btn-warning px-5
+              font-weight-bold" onClick={this.handleAdd}>add new task</button>
+            </div>
+            <div className="container-fluid">
+              <ul className="list-unstyled row m-5">
+              
+             { 
+                 this.state.items.map((value, i)=>{
+                   return <Task key={i} id= {i} value=
+                   {value} sendData={this.handleDelete}
+                   />
+                 })
+             }
+          
+              </ul>
+            </div>
+          </div>
+     
+
+
+      </div>
+      </div>
+      </div>
+    )
+  }
 }
 
 export default App;
